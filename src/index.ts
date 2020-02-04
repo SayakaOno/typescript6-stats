d736c2ac930ebb1b0c6b55ceb7bb80fc8f678fd0
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-02-04 08:00:47 -0800

    extract CsvFileReader

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === 'H') {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === 'A') {
     manUnitedWins++;
   }
 }
-
-console.log(`Man United won ${manUnitedWins} games`);
 
 console.log(`Man United won ${manUnitedWins} games`);
